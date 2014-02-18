6ea62677f9d53549f9c5545a31ad059830bf5428
seckcoder <seckcoder@gmail.com>
2014-02-18 15:41:52 +0800

    add more test case for closure

--- racket/compiler/scheme/tests-proc.scm
+++ racket/compiler/scheme/tests-proc.scm
@@ -8,26 +8,17 @@
             [f2 (proc (a b) (+ a (app-proc f1 b)))])
      (app-proc f2 3 4)) => "7\n"])|#
 
-#|(add-tests-with-string-output "closure"
-  [(let ([v 3])
-     ((lambda (u)
-        (+ v u)) v)) => "6\n"])|#
-
-
 (add-tests-with-string-output "closure"
-  [(labels
-     ((f (proc (env u) u)))
-     (app (closure f (vec)) 3))
-   => "3\n"]
   [(let ((u 3))
      (app (lambda (v)
-            (+ v u)) u)) => "6\n"])
-
-#|(add-tests-with-string-output "closure->proc"
-  [(labels
-    ((proc_162 (proc (env163 u)
-                     (+ (vec-ref env163 0) u))))
-    (let ((v 3))
-      (app-proc proc_162 (vec v) v)))
-      ;(app (closure proc_162 (vec v)) v)))
-   => "6\n"])|#
+            (+ v u)) u)) => "6\n"]
+  [(let* ((u 3)
+          (f (lambda (v)
+              (+ v u))))
+     (f 3)) => "6\n"]
+  [((lambda (v)
+      (app
+        (lambda (x)
+          (+ x v))
+        v)) 3) => "6\n"]
+  )
