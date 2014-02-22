11da2599923696bf61e6aeab6f5c818d471e5a3c
seckcoder <seckcoder@gmail.com>
2014-02-22 15:31:22 +0800

    update

--- racket/compiler/scheme/tests-vector.scm
+++ racket/compiler/scheme/tests-vector.scm
@@ -1,5 +1,5 @@
 (add-tests-with-string-output "vector"
-  #|[(let ([vec (make-vec 5)])
+  [(let ([vec (make-vec 5)])
      (vec-set! vec 0 1)
      (vec-ref vec 0)) => "1\n"]
   [(let ([vec (make-vec 5)])
@@ -15,6 +15,7 @@
   [(vec-ref (vec 1 2) 1) => "2\n"]
   [(length (vec 1 2 3)) => "3\n"]
   [(let ([vec (make-vec 5)])
-     (length vec)) => "5\n"]|#
+     (length vec)) => "5\n"]
   [(vec 1 2 3) => "3: [1, 2, 3]\n"]
+  [(make-vec 3) => "3: [0, 0, 0]\n"]
   )
